0160e7a598db7d72942a3dcc5ae810e66b5e84cd
User <User@DESKTOP-2R2JPTO>
2021-03-05 16:34:14 -0700

    changes

---
+++ MvcMovie/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        [Display(Name ="Genre")]
+        public string GenreName { get; set; }
+
+        public IList<Movie> Movie { get; set; }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name ="Released Date")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [Display(Name ="Genre")]
+        public int GenreId { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+
+        public string Image { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
